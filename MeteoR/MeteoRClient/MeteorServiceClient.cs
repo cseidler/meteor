dacc24aa754479db2a004155790af9b9c3fa8efd
Marc Hansen <marc.hansen@bbv.ch>
2014-06-23 16:08:37 +0200

    http client for wpf client

---
+++ MeteoR/MeteoRClient/MeteorServiceClient.cs
@@ -0,0 +1,25 @@
+﻿namespace MeteoRClient
+{
+    using System.IO;
+    using System.Net.Http;
+
+    using System.Threading.Tasks;
+
+    using MeteoRInterfaceModel;
+
+    public class MeteorServiceClient
+    {
+        private const string ServiceUri = "http://localhost:3074/api/weatherinfo";
+
+        public async Task<WeatherInfo> GetWeatherInfo(int id, long timestamp)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(Path.Combine(ServiceUri, string.Format("id={0}&timestamp={1}", id, timestamp))).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsAsync<WeatherInfo>().ConfigureAwait(false);
+            }
+        }
+    }
+}
