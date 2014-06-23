e72a340ec9cc49e2ae0f41c943deed0e9434dc53
Philipp <philipp.achermann@bbv.ch>
2014-06-23 16:39:12 +0200

    MeteoRClient: Renamed MainWindow to MainView and fixed namespace

---
+++ MeteoR/MeteoRClient/MainWindow/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MeteoRClient.MainWindow
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
