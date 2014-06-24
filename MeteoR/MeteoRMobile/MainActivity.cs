6a628b9da4c4b934742f150262a904485a22b7b2
ramon.spescha@bbv.ch <ramon.spescha@bbv.ch>
2014-06-24 08:40:05 +0200

    Anpassungen UI, neue Json Version 6.0.3

--- MeteoR/MeteoRMobile/MainActivity.cs
+++ MeteoR/MeteoRMobile/MainActivity.cs
@@ -1,27 +1,54 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Locations;
 using Android.Widget;
 using Android.OS;
+using MeteoRInterfaceModel;
 
 namespace MeteoRMobile
 {
     [Activity(Label = "MeteoRMobile", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private MeteorServiceClient service;
+        private Button receiveDataButton;
+        private TextView humidityData;
+        private TextView cityNameData;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            Button receiveDataButton = FindViewById<Button>(Resource.Id.receiveData);
+            receiveDataButton = FindViewById<Button>(Resource.Id.receiveData);
+
+            humidityData = FindViewById<TextView>(Resource.Id.humidityData);
+            cityNameData = FindViewById<TextView>(Resource.Id.cityNameData);
+
+            service = new MeteorServiceClient();
 
             // Get our button from the layout resource,
             // and attach an event to it
-            receiveDataButton.Click += (object sender, EventArgs e) =>
+            receiveDataButton.Click += ReceiveDataButtonOnClick;  
+            
+        }
+
+        private async void ReceiveDataButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            try
             {
-                // Translate user’s alphanumeric phone number to numeric
-                // receive Data
-            };
+                var weatherData = await this.service.GetWeatherInfo(1234, 1234657);
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(string.Format("Exception message: {0}", exception.Message));
+            }
+            
+
+            cityNameData.Text = "Moskau";
+            humidityData.Text = "10";
         }
     }
 }
--- MeteoR/MeteoRMobile/Resources/Resource.Designer.cs
+++ MeteoR/MeteoRMobile/Resources/Resource.Designer.cs
@@ -63,53 +63,56 @@ namespace MeteoRMobile
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060007
-			public const int cityNameData = 2131099655;
+			// aapt resource value: 0x7f060008
+			public const int cityNameData = 2131099656;
 			
-			// aapt resource value: 0x7f060006
-			public const int cityNameText = 2131099654;
+			// aapt resource value: 0x7f060007
+			public const int cityNameText = 2131099655;
 			
 			// aapt resource value: 0x7f060001
 			public const int gridView1 = 2131099649;
 			
-			// aapt resource value: 0x7f06000f
-			public const int humidityData = 2131099663;
+			// aapt resource value: 0x7f060010
+			public const int humidityData = 2131099664;
 			
-			// aapt resource value: 0x7f06000e
-			public const int humidityText = 2131099662;
+			// aapt resource value: 0x7f06000f
+			public const int humidityText = 2131099663;
 			
 			// aapt resource value: 0x7f060000
 			public const int meteorData = 2131099648;
 			
+			// aapt resource value: 0x7f06000e
+			public const int pressureData = 2131099662;
+			
 			// aapt resource value: 0x7f06000d
-			public const int pressureData = 2131099661;
-			
-			// aapt resource value: 0x7f06000c
-			public const int pressureText = 2131099660;
-			
-			// aapt resource value: 0x7f060002
-			public const int receiveData = 2131099650;
-			
-			// aapt resource value: 0x7f060005
-			public const int stationIdData = 2131099653;
-			
-			// aapt resource value: 0x7f060004
-			public const int stationIdText = 2131099652;
-			
-			// aapt resource value: 0x7f06000b
-			public const int temperatureData = 2131099659;
-			
-			// aapt resource value: 0x7f06000a
-			public const int temperatureText = 2131099658;
+			public const int pressureText = 2131099661;
 			
 			// aapt resource value: 0x7f060003
-			public const int textView1 = 2131099651;
+			public const int receiveData = 2131099651;
+			
+			// aapt resource value: 0x7f060006
+			public const int stationIdData = 2131099654;
+			
+			// aapt resource value: 0x7f060002
+			public const int stationIdInput = 2131099650;
+			
+			// aapt resource value: 0x7f060005
+			public const int stationIdText = 2131099653;
+			
+			// aapt resource value: 0x7f06000c
+			public const int temperatureData = 2131099660;
+			
+			// aapt resource value: 0x7f06000b
+			public const int temperatureText = 2131099659;
+			
+			// aapt resource value: 0x7f060004
+			public const int textView1 = 2131099652;
+			
+			// aapt resource value: 0x7f06000a
+			public const int timestampData = 2131099658;
 			
 			// aapt resource value: 0x7f060009
-			public const int timestampData = 2131099657;
-			
-			// aapt resource value: 0x7f060008
-			public const int timestampText = 2131099656;
+			public const int timestampText = 2131099657;
 			
 			static Id()
 			{
@@ -169,8 +172,6 @@ namespace MeteoRMobile
 			
 			private Style()
 			{
-			}
-		}
 			}
 		}
 	}
