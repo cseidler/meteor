00b3dbb0b75bd5173fe86ad947c880c2342ab29e
Marc Hansen <marc.hansen@bbv.ch>
2014-06-25 16:02:34 +0200

    added registered station api

--- MeteoR/MeteoRInterfaceModel/Station.cs
+++ MeteoR/MeteoRInterfaceModel/Station.cs
@@ -1,16 +1,24 @@
 ﻿namespace MeteoRInterfaceModel
 {
+    using System.Collections.Generic;
+
     public class Station
     {
         public int Id { get; set; }
 
         public string CityName { get; set; }
+    }
 
-        public override bool Equals(object obj)
+    public class StationEqualityComparer : IEqualityComparer<Station>
+    {
+        public bool Equals(Station x, Station y)
         {
-            Station station = (Station)obj;
+            return x.Id == y.Id && x.CityName == y.CityName;
+        }
 
-            return station != null && station.Id == this.Id && station.CityName == this.CityName;
+        public int GetHashCode(Station obj)
+        {
+            return (obj.Id + obj.CityName).GetHashCode();
         }
     }
 }
